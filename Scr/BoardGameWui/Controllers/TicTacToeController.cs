511469874fd8e7d3dd9f3ac85582d4d8f51f0675
Mikael <mikaelkarlgren@hotmail.se>
2018-04-26 21:44:14 +0200

    Player join support (doesn't save as a cookie as of yet)

--- Scr/BoardGameWui/Controllers/TicTacToeController.cs
+++ Scr/BoardGameWui/Controllers/TicTacToeController.cs
@@ -9,12 +9,34 @@ namespace BoardGameWui.Controllers
 {
     public class TicTacToeController : Controller
     {
-        // GET: TicTacToe
+
+        [HttpGet]
         public ActionResult Index()
         {
             var model = new GameModel();
-            model.Message = "Welcome to my Tic-Tac-Toe game. Play below with a friend using a secondary browser and try get three in a row!";
+            model.Message = "Welcome to my Tic-Tac-Toe game. Play below with a friend and try get three in a row! " +
+                "You need to play the game on the same computer, use two different browsers and play on one browser each.";
             return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Player model)
+        {
+            if (ModelState.IsValid)
+            {
+                AddPlayer(model.Name);
+                TempData["notice"] = "Player " + model.Name + " joined the game.";
+            }
+            else
+            {
+                TempData["notice"] = "You need to enter a valid name.";
+            }
+            return RedirectToAction("Index");
+        }
+
+        private void AddPlayer(string name)
+        {
+
         }
     }
 }
