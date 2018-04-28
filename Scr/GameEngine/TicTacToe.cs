f84eb9ab44651c89786db2886dc57be399aea72a
Mikael <mikaelkarlgren@hotmail.se>
2018-04-28 23:01:41 +0200

    Added #IsGameFinished method and corrected the coordinations of tiles on the gameboard

--- Scr/GameEngine/TicTacToe.cs
+++ Scr/GameEngine/TicTacToe.cs
@@ -27,11 +27,6 @@ namespace GameEngine
             }
         }
 
-        public bool IsPlayerOne(string name)
-        {
-            return name == Players[0];
-        }
-
         public TicTacToe()
         {
             ResetBoard();
@@ -41,6 +36,41 @@ namespace GameEngine
         public void PlaceMark(Mark mark, int index)
         {
             GameTiles[index].Mark = mark;
+        }
+
+        public bool IsGameFinished() // if three of the same mark is in a line
+        {
+            if (MarkAt(1, 1) != Mark.Available && // if diagonal line with three in a row of the same mark
+                (MarkAt(1, 1) == MarkAt(0, 2) && MarkAt(1, 1) == MarkAt(2, 0) || MarkAt(1, 1) == MarkAt(2, 0) && MarkAt(1, 1) == MarkAt(0, 2)))
+            {
+                return true;
+            }
+            for (int i = 0; 3 > i; i++)
+            {
+                if (MarkAt(i, 0) != Mark.Available && MarkAt(i, 0) == MarkAt(i, 1) && MarkAt(i, 0) == MarkAt(i, 2) ||
+                    MarkAt(0, i) != Mark.Available && MarkAt(0, i) == MarkAt(1, i) && MarkAt(0, i) == MarkAt(2, i))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private Mark MarkAt(int x, int y)
+        {
+            foreach (GameBoardTile tile in GameTiles)
+            {
+                if (tile.Coordinate.X == x & tile.Coordinate.Y == y)
+                {
+                    return tile.Mark;
+                }
+            }
+            return Mark.Available;
+        }
+
+        public bool IsPlayerOne(string name)
+        {
+            return name == Players[0];
         }
 
         public void AddPlayer(string name)
@@ -87,9 +117,9 @@ namespace GameEngine
         {
             GameTiles = new GameBoardTile[9];
             int index = 0;
-            for (int x = 0; 3 > x; x++)
+            for (int y = 2; y >= 0; y--)
             {
-                for (int y = 0; 3 > y; y++)
+                for (int x = 0; 3 > x; x++)
                 {
                     GameTiles[index] = new GameBoardTile(new Point(x, y), Mark.Available);
                     index++;
